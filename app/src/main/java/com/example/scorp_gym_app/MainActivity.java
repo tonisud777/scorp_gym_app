9828baade6c4cf59d8326b8e53458adbfbf50fc3
tonisud777 <antonrodaragon@gmail.com>
2021-02-17 19:54:45 -0600

    commiting this

--- app/src/main/java/com/example/scorp_gym_app/MainActivity.java
+++ app/src/main/java/com/example/scorp_gym_app/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//this is the last time I try
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
