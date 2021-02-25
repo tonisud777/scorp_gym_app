ed74ce6f9a36fc8615e6b3e9d72629684515db11
Rayshorn Richardson <rayshorndc@gmail.com>
2021-02-24 22:33:07 -0400

    We did the test methods

--- app/src/main/java/com/example/scorp_gym_app/MainActivity.java
+++ app/src/main/java/com/example/scorp_gym_app/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.scorp_gym_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 //this is the last time I try
 //this is the last time I try2.0
 
 public class MainActivity extends AppCompatActivity {
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
     }
     //What's uuuup!!!!!!
     //What's uuuup!!!!!!2.0
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    Intent intent = new Intent();
+
+    public boolean testIntent() {
+        if(intent == null){
+            System.out.println("The result is false");
+            return false;
+        }
+        System.out.println("The result is true");
+        return true;
+    }
+
 
 
 
